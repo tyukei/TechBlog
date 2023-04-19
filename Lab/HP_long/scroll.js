20936146715315da9c4606042809d92ff4348912
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-04-20 08:56:40 +0900

    add

---
+++ Lab/HP_long/scroll.js
@@ -0,0 +1,25 @@
+/* smooth scroll */
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
+
+/* active navigation item */
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrollDistance = $(window).scrollTop();
+        $('section').each(function (i) {
+            if ($(this).position().top <= scrollDistance + 100) {
+                $('nav ul li.active').removeClass('active');
+                $('nav ul li').eq(i).addClass('active');
+            }
+        });
+    }).scroll();
+});
