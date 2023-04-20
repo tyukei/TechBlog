52ca21793278dfebd75dc62c054f60f881ac192a
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-04-20 14:50:52 +0900

    add: slider

---
+++ Lab/HP_long/slideshow.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        pauseOnHover: false,
+        centerMode: true,
+        centerPadding: "20%",
+    });
+});
