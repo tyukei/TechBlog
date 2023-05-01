05ba28c9aa61a4df101a50f951bb3d15f0cba327
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-05-01 10:11:51 +0900

    add

---
+++ Lab/HP_long/Particle/particle2.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    //ページ内スクロール
+    var navHeight = $(".header").outerHeight();
+
+    $('a[href^="#"]').on("click", function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? "html" : href);
+        var position = target.offset().top - navHeight;
+        $("html, body").animate({ scrollTop: position, }, 300, "swing");
+        return false;
+    });
+
+    //ページトップ
+    $("#js-page-top").on("click", function () {
+        $("body,html").animate({ scrollTop: 0, }, 300);
+        return false;
+    });
+
+});
+
+
+
+window.onload = function () {
+    Particles.init(
+        {
+            selector: '.particle-background',
+            maxParticles: 200,
+            sizeVariations: 3,
+            speed: 1,
+            color: ['#D0D0D0', '#BBBBBB', '#EEEEEE'],
+            connectParticles: true
+        }
+    );
+};
---
+++ Lab/HP_long/Particle/particle3.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    //ページ内スクロール
+    var navHeight = $(".header").outerHeight();
+
+    $('a[href^="#"]').on("click", function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? "html" : href);
+        var position = target.offset().top - navHeight;
+        $("html, body").animate({ scrollTop: position, }, 300, "swing");
+        return false;
+    });
+
+    //ページトップ
+    $("#js-page-top").on("click", function () {
+        $("body,html").animate({ scrollTop: 0, }, 300);
+        return false;
+    });
+
+});
+
+
+
+window.onload = function () {
+    Particles.init(
+        {
+            selector: '.particle-background',
+            maxParticles: 80,
+            sizeVariations: 30,
+            speed: 3,
+            color: ['#D0D0D0', '#FFE5B4', '#BBBBBB'],
+            connectParticles: false
+        }
+    );
+};
